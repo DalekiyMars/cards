dc22c9a322eba5cdfa8a5b4d59a8233b8c248587
Eugene <evgeniybulychev@bk.ru>
2026-01-09 12:10:30 +0300

    swagger config

---
+++ src/test/java/com/banking/cards/constants/TestConstants.java
@@ -0,0 +1,6 @@
+package com.banking.cards.constants;
+
+public class TestConstants {
+    public static final String BASE_CONTROLLER_PATH = "src/test/resources/controller";
+    public static final String BASE_SERVICE_PATH = "src/test/resources/service";
+}
